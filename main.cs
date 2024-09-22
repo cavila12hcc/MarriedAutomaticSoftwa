ff062aeccb19b9cf547e9f5fdaae0b6e65b0cd6e
Carlos Avila <35896114-cavila12@users.noreply.replit.com>
2024-09-22 22:16:21 +0000

    fin

--- main.cs
+++ main.cs
@@ -1,7 +1,25 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static string PostMessage(string message)
+    {
+        if (message.Length <= 140)
+        {
+            return "Posted";
+        }
+        else
+        {
+            return "Rejected";
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your message:");
+        string userMessage = Console.ReadLine();
+
+        string result = PostMessage(userMessage);
+        Console.WriteLine(result);
+    }
 }
